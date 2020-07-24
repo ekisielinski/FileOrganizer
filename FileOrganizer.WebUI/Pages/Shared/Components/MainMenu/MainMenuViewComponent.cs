fd807983ea92e4322fe35404b83ae1c310ad1853
null <fake@fake12345.com>
2020-07-24 12:19:55 +0200

    Added empty MainMenu view component

---
+++ FileOrganizer.WebUI/Pages/Shared/Components/MainMenu/MainMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileOrganizer.WebUI.Pages.Shared.Components.MainMenu
+{
+    public sealed class MainMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
