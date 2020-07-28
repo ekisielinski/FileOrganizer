161e60d3a6de425029239503fafc4955dfc58059
null <fake@fake12345.com>
2020-07-28 19:53:06 +0200

    StaticFilesController can now read files from Database folder

--- FileOrganizer.WebUI/Controllers/StaticFilesController.cs
+++ FileOrganizer.WebUI/Controllers/StaticFilesController.cs
@@ -1,20 +1,63 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FileOrganizer.CommonUtils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using System.Linq;
 
 namespace FileOrganizer.WebUI.Controllers
 {
-    [Route( "static/{action}/{id}" )]
+    [Route( "static/{action}/{fileName}" )]
     [Authorize]
     public class StaticFilesController : Controller
     {
-        public string File( int id )
+        readonly IWebHostEnvironment webHostEnvironment;
+
+        //====== ctors
+
+        public StaticFilesController( IWebHostEnvironment webHostEnvironment )
         {
-            return "file id: " + id;
+            this.webHostEnvironment = Guard.NotNull( webHostEnvironment, nameof( webHostEnvironment ) );
         }
 
-        public string Thumb( int id )
+        //====== private properties
+
+        private IDirectoryContents FilesDir  => webHostEnvironment.ContentRootFileProvider.GetDirectoryContents( "/Database/Files" );
+        private IDirectoryContents ThumbsDir => webHostEnvironment.ContentRootFileProvider.GetDirectoryContents( "/Database/Thumbs" );
+
+        //====== actions
+
+        public IActionResult File( string fileName )
         {
-            return "thumb id: " + id;
+            IFileInfo file = FilesDir.FirstOrDefault( x => x.Name == fileName );
+
+            return FileInfoToActionResult( file );
+        }
+
+        public IActionResult Thumb( string fileName )
+        {
+            IFileInfo file = ThumbsDir.FirstOrDefault( x => x.Name == fileName );
+
+            return FileInfoToActionResult( file );
+        }
+
+        //====== private methods
+
+        private IActionResult FileInfoToActionResult( IFileInfo file )
+        {
+            if (file is null) return NotFound();
+
+            // TODO: should return file if mime type is supported by the browser, othwerise should return fixed icon(?)
+
+            var provider = new FileExtensionContentTypeProvider();
+
+            if (provider.TryGetContentType( file.Name, out string contentType ))
+            {
+                return PhysicalFile( file.PhysicalPath, contentType );
+            }
+
+            return NotFound();
         }
     }
 }
