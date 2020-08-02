afb77da5ac6eebc583a4cf880ea8f652db7fab36
null <fake@fake12345.com>
2020-08-02 10:24:41 +0200

    Added struct: DataSize

---
+++ FileOrganizer.CommonUtils/Code/DataSize.cs
@@ -0,0 +1,18 @@
+﻿namespace FileOrganizer.CommonUtils
+{
+    public readonly struct DataSize
+    {
+        public DataSize( long bytes )
+        {
+            Bytes = bytes;
+        }
+
+        //====== public properties
+
+        public long Bytes { get; }
+
+        //====== override: Object
+
+        public override string ToString() => Bytes.ToString();
+    }
+}
