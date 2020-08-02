0873b5b1e2008dbe7993421ecb6123d99d5e7b1b
null <fake@fake12345.com>
2020-08-02 17:09:09 +0200

    Added StaticFilesLinkGenerator

---
+++ FileOrganizer.WebUI/Services/IStaticFilesLinkGenerator.cs
@@ -0,0 +1,9 @@
+﻿using FileOrganizer.Services.FileDatabase;
+
+namespace FileOrganizer.WebUI.Services
+{
+    public interface IStaticFilesLinkGenerator
+    {
+        string GetDatabaseFilePath( FileName fileName, FileDatabaseFolder folder );
+    }
+}
---
+++ FileOrganizer.WebUI/Services/StaticFilesLinkGenerator.cs
@@ -0,0 +1,45 @@
+﻿using FileOrganizer.CommonUtils;
+using FileOrganizer.Services.FileDatabase;
+using FileOrganizer.WebUI.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace FileOrganizer.WebUI.Services
+{
+    public sealed class StaticFilesLinkGenerator : IStaticFilesLinkGenerator
+    {
+        readonly LinkGenerator linkGenerator;
+        readonly IHttpContextAccessor accessor;
+
+        //====== ctors
+
+        public StaticFilesLinkGenerator( LinkGenerator linkGenerator, IHttpContextAccessor accessor )
+        {
+            this.linkGenerator = Guard.NotNull( linkGenerator, nameof( linkGenerator ) );
+            this.accessor = Guard.NotNull( accessor, nameof( accessor ) );
+        }
+
+        //====== IStaticFilesLinkGenerator
+
+        public string GetDatabaseFilePath( FileName fileName, FileDatabaseFolder folder )
+        {
+            Guard.NotNull( fileName, nameof( fileName ) );
+
+            string actionName = folder switch
+            {
+                FileDatabaseFolder.Files  => nameof( StaticFilesController.File ),
+                FileDatabaseFolder.Thumbs => nameof( StaticFilesController.Thumb ),
+
+                _ => throw new ArgumentException( "Invalid enum value.", nameof( folder ) )
+            };
+
+            return linkGenerator.GetPathByAction(
+                httpContext: accessor.HttpContext,
+                action:      actionName,
+                controller:  "StaticFiles", // TODO: controller name utils...
+                values:      new { fileName = fileName.Value }
+                );
+        }
+    }
+}
