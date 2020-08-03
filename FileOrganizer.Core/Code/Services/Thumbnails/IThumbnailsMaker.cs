aaf045f2caa656982dcbcad040d0d6bdb9e7a1fc
null <fake@fake12345.com>
2020-08-03 11:10:08 +0200

    Added service for generating thumbnails

---
+++ FileOrganizer.Core/Code/Services/Thumbnails/IThumbnailsMaker.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.Drawing;
+
+namespace FileOrganizer.Core.Services
+{
+    public interface IThumbnailsMaker
+    {
+        Image? MakeThumb( IFileInfo fileInfo, Size size );
+    }
+}
---
+++ FileOrganizer.Core/Code/Services/Thumbnails/ThumbnailsMaker.cs
@@ -0,0 +1,16 @@
+﻿using FileOrganizer.Core.Utils;
+using Microsoft.Extensions.FileProviders;
+using System.Drawing;
+
+namespace FileOrganizer.Core.Services
+{
+    public sealed class ThumbnailsMaker : IThumbnailsMaker
+    {
+        public Image? MakeThumb( IFileInfo fileInfo, Size size )
+        {
+            using var stream = fileInfo.CreateReadStream();
+
+            return ImageThumbnailsMaker.MakeThumb( stream, size );
+        }
+    }
+}
