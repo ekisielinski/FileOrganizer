853fdd3e393be089f1a70949dafdeede243d8be9
null <fake@fake12345.com>
2020-08-10 14:42:48 +0200

    Added LoginStatus view component

---
+++ FileOrganizer.WebUI/Pages/Shared/Components/LoginStatus/LoginStatusViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using FileOrganizer.CommonUtils;
+using FileOrganizer.WebUI.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileOrganizer.WebUI.Pages.Shared.Components.LoginStatus
+{
+    public sealed class LoginStatusViewComponent : ViewComponent
+    {
+        readonly IAuthUserAccessor accessor;
+
+        //====== ctors
+
+        public LoginStatusViewComponent( IAuthUserAccessor accessor )
+        {
+            this.accessor = Guard.NotNull( accessor, nameof( accessor ) );
+        }
+
+        //====== view component
+
+        public IViewComponentResult Invoke()
+        {
+            AuthUser? model = accessor.CurrentUser;
+
+            return View( model );
+        }
+    }
+}
