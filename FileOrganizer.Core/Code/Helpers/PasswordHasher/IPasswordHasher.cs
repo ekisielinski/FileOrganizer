c57029082da92d2dc49f23231c3c89d425f3609e
null <fake@fake12345.com>
2020-09-07 19:15:36 +0200

    Added password hasher

---
+++ FileOrganizer.Core/Code/Helpers/PasswordHasher/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace FileOrganizer.Core
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword( UserPassword password );
+
+        bool VerifyHash( string hash, UserPassword password );
+    }
+}
---
+++ FileOrganizer.Core/Code/Helpers/PasswordHasher/PasswordHasher.cs
@@ -0,0 +1,28 @@
+﻿using FileOrganizer.CommonUtils;
+using Microsoft.AspNetCore.Identity;
+
+namespace FileOrganizer.Core
+{
+    public sealed class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword( UserPassword password )
+        {
+            Guard.NotNull( password, nameof( password ) );
+
+            var hasher = new PasswordHasher<object>();
+
+            return hasher.HashPassword( new object(), password.Value );
+        }
+
+        public bool VerifyHash( string hash, UserPassword password )
+        {
+            Guard.NotNull( hash, nameof( hash ) );
+            Guard.NotNull( password, nameof( password ) );
+
+            var hasher = new PasswordHasher<object>();
+            var result = hasher.VerifyHashedPassword( new object(), hash, password.Value );
+
+            return result != PasswordVerificationResult.Failed;
+        }
+    }
+}
