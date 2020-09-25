b2503023e3c73d963f99286635f48baa24b659ec
null <fake@fake12345.com>
2020-09-25 18:30:46 +0200

    Added FeatureManagement, ability to disable Upload functionality

--- FileOrganizer.WebUI/Startup.cs
+++ FileOrganizer.WebUI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
 
 namespace FileOrganizer.WebUI
 {
@@ -30,6 +31,7 @@ namespace FileOrganizer.WebUI
                 options.Conventions.AuthorizeFolder( "/" );
             } );
 
+            services.AddFeatureManagement();
             services.AddHttpContextAccessor();
             services.AddTransient<IStaticFilesLinkGenerator, StaticFilesLinkGenerator>();
             services.AddTransient<IDatabaseInitializer, DefaultDatabaseInitializer>();
