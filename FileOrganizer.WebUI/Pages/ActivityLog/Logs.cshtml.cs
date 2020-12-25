f04c6b75cc32696d55dd594616dc5d306bc10e32
null <fake@fake12345.com>
2020-12-25 15:28:21 +0100

    Activity log: added filtering by user name

--- FileOrganizer.WebUI/Pages/ActivityLog/Logs.cshtml.cs
+++ FileOrganizer.WebUI/Pages/ActivityLog/Logs.cshtml.cs
@@ -1,23 +1,46 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FileOrganizer.Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FileOrganizer.WebUI.Pages
 {
     public class LogsModel : PageModel
     {
-        public IReadOnlyList<ActivityLogEntry> Logs { get; private set; } = new List<ActivityLogEntry>();
+        public IReadOnlyList<ActivityLogEntry> Logs      { get; private set; } = new List<ActivityLogEntry>();
+        public SelectList                      UsersList { get; private set; } = new SelectList( Enumerable.Empty<object>() );
+
+        public string? ReloadUriBase { get; private set; }
 
         //====== actions
 
-        public async Task OnGetAsync( [FromServices] ISender sender )
+        public async Task OnGetAsync( [FromQuery] string? userNameFilter, [FromServices] ISender sender )
         {
-            GetActivityLogEntriesQuery query = new();
+            IReadOnlyList<AppUser> appUsers = await sender.Send( new GetAppUsersQuery() );
+
+            AppUser? currentAppUser = null;
+
+            if (userNameFilter is not null)
+            {
+                currentAppUser = appUsers.FirstOrDefault( x => x.Name.Value == userNameFilter );
+
+                if (currentAppUser is null) throw new( "User not found." ); // TODO: custom ex
+            }
+
+            UsersList = new SelectList( appUsers, nameof( AppUser.Name ), nameof( AppUser.Name ), currentAppUser?.Name?.Value );
+
+            GetActivityLogEntriesQuery query = new()
+            {
+                UserNameFilter = userNameFilter is null ? null : new UserName( userNameFilter )
+            };
 
             Logs = await sender.Send( query );
+
+            ReloadUriBase = Url.PageLink( "/Logs" ) + "?" + nameof( userNameFilter ) + "=";
         }
     }
 }
